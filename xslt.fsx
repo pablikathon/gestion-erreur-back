8f43ddd3082221588db0c5e57ed707b067cab8fe
Horizons-group <support@horizons-group.com>
2024-08-15 13:46:42 +0200

    print error

--- xslt.fsx
+++ xslt.fsx
@@ -1,24 +1,11 @@
-open System.IO;
-open System.Xml.Xsl;
+open System
+open System.Xml
+open System.Xml.Xsl
 
-if fsi.CommandLineArgs.Length <> 4
-then
-    printfn "wrong command, please use :"
-    printfn "dotnet fsi xslt.fsx {inputXML} {xslt file} {outputfile}"
-else    
-
-    let inputfile = Path.Combine(__SOURCE_DIRECTORY__, fsi.CommandLineArgs.[1])
-    let transform = Path.Combine(__SOURCE_DIRECTORY__, fsi.CommandLineArgs.[2])
-    let outputfile = Path.Combine(__SOURCE_DIRECTORY__, fsi.CommandLineArgs.[3])
-
-
-    if not <| File.Exists(inputfile)
-    then  printfn $"File not found: {inputfile}"
-
-    if not <| File.Exists(transform)
-    then  printfn $"File not found: {transform}"
-
-    let xslTransform = new XslTransform();
-    xslTransform.Load(transform);
-    xslTransform.Transform(inputfile, outputfile);
-    printfn $"Finished, output: {outputfile}"
+try
+    let transform = new XslCompiledTransform()
+    transform.Load("ic.xslt")
+    transform.Transform("inspectcode.xml", "inspectcode.html")
+    printfn "Transformation completed successfully."
+with
+| ex -> printfn "An error occurred: %s" (ex.Message)
