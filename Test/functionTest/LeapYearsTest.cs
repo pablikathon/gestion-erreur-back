49d00353c9f09820833766a33172eefad8935cda
llayton <lyes.souifi@gmail.com>
2024-08-18 20:43:42 +0200

    test fix

--- Test/functionTest/LeapYearsTest.cs
+++ Test/functionTest/LeapYearsTest.cs
@@ -6,7 +6,7 @@ public class LeapYearTests
     [Theory]
     [InlineData(1900)]
     [InlineData(2021)]
-    [InlineData(2020)]
+    [InlineData(2023)]
 
     public void Should_not_be_a_leap_year(int year)
     {
