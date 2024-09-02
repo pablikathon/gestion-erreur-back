ac5e89104c50ea4cfd1978544d59e3348033e351
llayton <lyes.souifi@gmail.com>
2024-09-02 14:30:02 +0200

    jetbrain suggestion : createdType is evident

--- Services/Model/common/search/QueryParameter.cs
+++ Services/Model/common/search/QueryParameter.cs
@@ -3,9 +3,9 @@ namespace Services.Models.Common;
 public class QueryParameters
 {
 
-    public DateParameters DateParam { get; set; } = new DateParameters();
+    public DateParameters DateParam { get; set; } = new ();
     public SearchParameters?  SearchParam { get; set;}
 
-    public PaginationParameters Pagination { get; set; } = new PaginationParameters();
-    public SortParameters Sort { get; set; } = new SortParameters();
+    public PaginationParameters Pagination { get; set; } = new ();
+    public SortParameters Sort { get; set; } = new ();
 }
