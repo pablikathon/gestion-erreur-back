b8e80aa922f7b0c94286d356a086c41980fe297e
llayton <lyes.souifi@gmail.com>
2024-09-03 17:21:47 +0200

    remove cycle reference

--- Persist/Entity/LookupTable/ErrorStatus.cs
+++ Persist/Entity/LookupTable/ErrorStatus.cs
@@ -1,10 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Persist.Entities
 {
     public class ErrorStatusEntity
     {
         public required string Id { get; set; }
         public required string Title { get; set; }
-
+        [JsonIgnore]
         public ICollection<ErrorEntity>? Errors { get; set; }
     }
 }
--- Persist/Entity/LookupTable/SeverityLevel.cs
+++ Persist/Entity/LookupTable/SeverityLevel.cs
@@ -1,10 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Persist.Entities
 {
     public class SeverityLevelEntity
     {
         public required string Id { get; set; }
         public required string Title { get; set; }
-
+        [JsonIgnore]
         public ICollection<ErrorEntity>? Errors { get; set; }
     }
 }
